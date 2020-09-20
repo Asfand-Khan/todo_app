7eac145d861c7feee1dd5211957dffbeb7d71e6f
Asfand-Khan <khanasfand174@gmail.com>
2020-09-20 12:18:29 +0500

    update App.js

--- src/Todo.js
+++ src/Todo.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {ListItem,ListItemText,Button} from '@material-ui/core';
+import './Todo.css';
 import db from './firebase';
 
 function Todo(props) {
     return (
-        <div>
+        <div className='todo'>
             <ListItem>
-            <ListItemText primary={props.todo.todo} secondary="Dummy Dead line" />
+            <ListItemText primary={props.todo.todo} secondary="Made by Asfand Khan" className='todo__input' />
             <Button color='secondary' variant='contained' onClick={e => db.collection('todos').doc(props.todo.id).delete()}>Delete Me</Button>
             </ListItem>
         </div>
