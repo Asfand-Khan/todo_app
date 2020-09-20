bbfa28178833df63fe4a79b9df6b4714c9fe679b
Asfand-Khan <khanasfand174@gmail.com>
2020-09-20 11:51:46 +0500

    add delete funtiionality

--- src/Todo.js
+++ src/Todo.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {ListItem,ListItemText,Button} from '@material-ui/core';
+import db from './firebase';
 
 function Todo(props) {
     return (
         <div>
             <ListItem>
             <ListItemText primary={props.todo.todo} secondary="Dummy Dead line" />
-            <Button color='secondary' variant='contained'>Delete Me</Button>
+            <Button color='secondary' variant='contained' onClick={e => db.collection('todos').doc(props.todo.id).delete()}>Delete Me</Button>
             </ListItem>
         </div>
     )
